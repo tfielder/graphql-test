7ea72184c268eaa654ccf5a62e0291482210cb4e
Tim Fielder <timfielderdev@gmail.com>
2024-10-09 16:14:42 -0600

    add authors field to query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -39,5 +39,11 @@ module Types
     def author(id:)
       Author.where(id: id).first
     end
+
+    field :authors, [Types::AuthorType], null: false
+
+    def authors
+      Author.all
+    end
   end
 end
