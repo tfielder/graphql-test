8534b1ddfe85fb80509990369f8bf49140bcfb78
Tim Fielder <timfielderdev@gmail.com>
2024-10-09 15:36:38 -0600

    add author type and associated fields

---
+++ app/graphql/types/author_type.rb
@@ -0,0 +1,9 @@
+class Types::AuthorType < Types::BaseObject
+    description "one author"
+
+    field :id, ID, null: false
+    field :first_name, String, null: true #, camelize: false #can use camelize false to ensure not camelizing on the return value
+    field :last_name, String, null: true
+    field :yob, Integer, null: true
+    field :is_alive, Boolean, null: true
+end
