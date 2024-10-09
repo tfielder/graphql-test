21f30d15306027c2fbaaa8f7f263124d72158fbf
Tim Fielder <timfielderdev@gmail.com>
2024-10-09 14:27:16 -0600

    update field to use an argument

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -23,9 +23,11 @@ module Types
 
     # TODO: remove me
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+      description: "An example field added by the generato 456789" do
+        argument :name, String, required: true
+      end
+    def test_field(name:)
+      "Hello #{name}!"
     end
   end
 end
