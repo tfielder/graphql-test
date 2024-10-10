210b225e1fbada54d39b9331148f7e81f4e7dfdd
Tim Fielder <timfielderdev@gmail.com>
2024-10-10 15:30:10 -0600

    add example of adding a mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,11 +2,18 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    # field :create_author, Types::AuthorType, null: true, description: "Create an author" do
+    #   argument :first_name, String, required: false
+    #   argument :last_name, String, required: false
+    #   argument :yob, Int, required: false
+    #   argument :is_alive, Boolean, required: false
+    # end
+
+    # def create_author(first_name:, last_name:, yob:, is_alive:)
+    #   Author.create first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive
+    # end
+  
+    #alternative way to create a mutation
+    field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor #indicates external file defining
   end
 end
