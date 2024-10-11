96252ec0fa47f02d65c579589c5c533348593985
Tim Fielder <timfielderdev@gmail.com>
2024-10-11 16:05:45 -0600

    remove puts statement on author

--- app/graphql/mutations/create_author.rb
+++ app/graphql/mutations/create_author.rb
@@ -15,7 +15,6 @@ class Mutations::CreateAuthor < GraphQL::Schema::Mutation
     argument :author, Types::AuthorInputType, required: true
 
     def resolve(author:)
-        puts "author is #{author}"
         Author.create author.to_h
     end
 end
